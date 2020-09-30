912d599f2bca0a60b507c6bf620f686d2e1a1618
betusG <66532814+betusG@users.noreply.github.com>
2020-09-30 21:41:10 +0100

    i have updated it my java file

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,6 +1,9 @@
 public class Hello {
 
     public static void main(String[] args) {
-        System.out.println("Hello Class");
+        System.out.println("Hello Class EU3");
+
+        //more changes are comings
+        //i am ready to commit tis
     }
 }
